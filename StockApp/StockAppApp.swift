349cda260ce2b9b549e93cb3d3824aad37b02aaa
Ankit Sharma <ankitsoftengi@gmail.com>
2023-09-14 20:35:34 +0530

    Initial Commit

---
+++ StockApp/StockAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockAppApp.swift
+//  StockApp
+//
+//  Created by Ankit Sharma on 14/09/23.
+//
+
+import SwiftUI
+
+@main
+struct StockAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
